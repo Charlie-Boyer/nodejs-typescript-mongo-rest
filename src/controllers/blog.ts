2ca4deb853092a2c4bb8d607930a075f86347d75
Charlie-Boyer <charlie.josuas@gmail.com>
2021-07-08 17:42:22 +0200

    add user model

--- src/controllers/blog.ts
+++ src/controllers/blog.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
+import User from '../models/user';
 
 const readAll = (req: Request, res: Response) => {
+  User.find();
   return res.send('hello');
 };
 
