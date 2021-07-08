0e768fa65f75a31273a993cfceff16d3646420d9
Charlie-Boyer <charlie.josuas@gmail.com>
2021-07-08 16:03:43 +0200

    add config and basic server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,16 @@
 import http from 'http';
 import express from 'express';
+import config from './config/config';
+import mongoose from 'mongoose';
 
 const router = express();
 const httpServer = http.createServer(router);
+
+/** Connect database */
+mongoose
+  .connect(config.mongo.url, config.mongo.options)
+  .then(() => console.log('connected'))
+  .catch((error) => console.log(`an error occured ${error}`));
+
+/** Server listen */
+httpServer.listen(config.server.port, () => 'server connected');
